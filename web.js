20465add827c51fcffc0d81954790d5df46d13ab
Ubuntu <ubuntu@ip-172-31-28-219.us-west-2.compute.internal>
2013-07-20 13:00:25 +0000

    added it

--- web.js
+++ web.js
@@ -1,23 +1,49 @@
 var express = require('express');
+
 var app = express.createServer(express.logger());
-var fs=require('fs');
-//var htmlfile="/index.html";
+
+
 app.get('/', function(request, response) {
+/*
+fs.readFileSync('/index.html', function (err, dat$|    at Interface.<anonymous> (repl.js:239:12)
+  if (err) throw err;             |    at Interface.EventEmitter.emit (events.js:$
+  var buffer = new Buffer(20);
+  buffer.write ("Hello", "utf-8")    |    at Interface._onLine (readline.js:202:10)
+}); 
+*/
 
-//var html=fs.readFileSync(htmlfile).toString();
 
-//if (err) { response.send ('error thrown!'); }
-//response.send(html); 
-fs.readFile('/index.html','utf-8',function (err,data) {
-if (err) { throw err; }
+fs = require('fs')
+fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  response.send(data);
+// console.log(data);
 
-//var filedata=data.toString()
-response.send(data);
 });
-//  response.send('Hello World2!');
+
+
+
+
+
+
+
+
+//var buffer = new Buffer(20);
+//buffer.write("hello","utf-8");
+    
+//  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+/*
+fs.readFile('/etc/passwd', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+*/
