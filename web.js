b9d06d7ed22f0843c7739335856d6c73e8abe27d
Ubuntu <ubuntu@ip-172-31-28-219.us-west-2.compute.internal>
2013-07-20 17:11:14 +0000

    small modif

--- web.js
+++ web.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var fs = require('fs'); 
 var app = express.createServer(express.logger());
-//var fs=require('fs');
-//var htmlfile="index.html";
 
 
 app.get('/', function(request, response) {
@@ -11,24 +9,8 @@ fs.readFileSync('index.html',function(err,data) {
 
 if (err) { return response.send(err); }
 data1=data.toString();
-response.send(data1);
+response.send(data);
 });
-
-
-/*
-var html=fs.readFileSync(htmlfile).toString();
-
-//if (err) { response.send ('error thrown!'); }
-response.send(html); 
-//fs.readFile('/index.html','utf-8',function (err,data) {
-//if (err) { throw err; }
-
-//var filedata=data.toString()
-//response.send(data);
-//});
-//  response.send('Hello World2!');
-
-*/
 
 });
 
